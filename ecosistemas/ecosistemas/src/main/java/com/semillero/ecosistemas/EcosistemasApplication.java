702ab788c5d7445aa96ad6ec76828816ee2cab41
mdev <matias.mazzitelli.dev@gmail.com>
2024-06-28 16:52:34 -0300

    Proyecto Base

---
+++ ecosistemas/ecosistemas/src/main/java/com/semillero/ecosistemas/EcosistemasApplication.java
@@ -0,0 +1,13 @@
+package com.semillero.ecosistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcosistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcosistemasApplication.class, args);
+	}
+
+}
